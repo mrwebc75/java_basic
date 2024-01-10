fdd5f4f69a97e875f56dc4d4ff658964331f29be
안쌤 <mvcweb@naver.com>
2024-01-10 15:01:29 +0900

    자바수업소스

--- extends_60/components/Unit.java
+++ extends_60/components/Unit.java
@@ -21,4 +21,5 @@ public class Unit {
   public void attack() {
 
   }
+
 }
---
+++ polymorphism_70/Ex10_CarMain.java
@@ -0,0 +1,16 @@
+package polymorphism_70;
+
+//작업지시서 역할의 추상클래스
+abstract class HCar {
+  public abstract void door();
+}
+
+class sonata extends HCar {
+}
+
+public class Ex10_CarMain {
+
+  public static void main(String[] args) {
+
+  }
+}
