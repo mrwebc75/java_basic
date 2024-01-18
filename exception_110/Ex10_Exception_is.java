64d01d8a5b120ec1f3c82098fa755177bf3ea0d6
안쌤 <mvcweb@naver.com>
2024-01-18 15:44:24 +0900

    java

--- exception_110/Ex10_Exception_is.java
+++ exception_110/Ex10_Exception_is.java
@@ -9,7 +9,7 @@ package exception_110;
      
      2. 논리오류 - 해결난이도 가장 높음
      
-     3. 실행(Runtime) 오류 - 실행조건에 따라 발생할 수 있는 오작동 -> 예외오류
+     3. 예외오류 - 실행조건에 따라 발생할 수 있는 오작동 -> 예외오류
          예) HDD고장, 메모리고장, 메모리부족, 인터넷끊김, 정전시 오작동
          
          논리적으로는 문제가 없는 코드이기는 하지만 ....
@@ -18,6 +18,18 @@ package exception_110;
          해결방법           
            만약 내가 작성하는 코드가 오류가 발생할 여지가 있는 코드라면
            try~catch~finally 구문을 작성한다. 
+           
+     4. Exception의 구분
+     
+         1) Checked 익셉션
+            -> 컴파일러가 예외처리 구문 작성을 강제로 지시함 
+            -> Class.forName("com.mysql.cj.jdbc.Driver");
+         
+         2) UnChecked 익셉션
+            -> 개발자가 필요에 의해 예외처리 구문을 작성(강제아님)
+            -> NumberFormatException, ArrayIndexOutOfBoundsException의 경우는 예외처리를 강제하지 않는다.
+            -> RuntimeException을 상속받은 익셉션의 경우 예외처리 구문 작성이 필수가 아니다.
+            -> 즉, 의무적으로 작성하지 않아도 에러로 잡히지 않는다. 
 */
 
 public class Ex10_Exception_is {
